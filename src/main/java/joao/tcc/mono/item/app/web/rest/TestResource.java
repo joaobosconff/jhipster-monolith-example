0d6bd80f23d4c6010616dd9768a4a03f1ce91572
João Bosco <joaobosconff03@gmail.com>
2021-09-20 17:09:24 -0300

    added endpoint to test cpu use

---
+++ src/main/java/joao/tcc/mono/item/app/web/rest/TestResource.java
@@ -0,0 +1,27 @@
+package joao.tcc.mono.item.app.web.rest;
+
+import java.util.ArrayList;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class TestResource {
+	
+	
+	@GetMapping("/matriz")
+	public void test(){
+	    CalculadoraMatriz.init(300);
+	}
+	
+	@GetMapping("/collection")
+	public ResponseEntity<ArrayList<Integer>> getSort(){
+		return ResponseEntity.ok(CollectionTest.init());
+	}
+
+    
+}
